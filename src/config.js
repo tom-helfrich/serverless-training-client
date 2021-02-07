0b2e4095b0fa02d8fbc343e579f7d78e4768f562
Thomas Helfrich <thomasmhelfrich@gmail.com>
2021-02-06 21:39:38 -0600

    Setting up configs and navbar for React client

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: 'us-east-2',
+    BUCKET: 'notes-app-uploads-february-2021',
+  },
+  apiGateway: {
+    REGION: 'us-east-2',
+    URL: 'https://mglsk704h1.execute-api.us-east-2.amazonaws.com/prod',
+  },
+  cognito: {
+    REGION: 'us-east-2',
+    USER_POOL_ID: 'us-east-2_mDcCHnc8I',
+    APP_CLIENT_ID: '4i2kpqidgd9ptqi9dg0cqvpj1s',
+    IDENTITY_POOL_ID: 'us-east-2:d3c01f38-c114-4337-b558-6c59bfea4ee5',
+  },
+};
+
+export default config;
